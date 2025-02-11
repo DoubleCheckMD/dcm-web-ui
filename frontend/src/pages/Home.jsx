7f35809aad38330f5aa1216363ba05e306ef0472
Netri shelat <shelatnetri92@gmail.com>
2025-02-11 11:40:39 -0500

    Home page redirect to upload page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import useFetchUser from '../utils/useFetchUser'; // Import the custom hook
-import UserProfileHeader from '../components/UserProfileHeader'; // Import the UserProfileHeader component
 
 const Home = () => {
   const navigate = useNavigate();
-  const user = useFetchUser(); // Use the custom hook to fetch user data
 
-  return (
-    <div>
-      <UserProfileHeader user={user} /> {/* Use the UserProfileHeader component */}
-      <div style={{ padding: '10px' }}>
-        <Link to="/upload" className="text-blue-500">Upload Photo</Link>
-      </div>
-    </div>
-  );
+  useEffect(() => {
+    navigate('/upload');
+  }, [navigate]);
+
+  return null; // Avoid rendering anything since it's redirecting
+  
 };
 
 export default Home;
