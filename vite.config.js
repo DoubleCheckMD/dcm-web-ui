d344aff0858b42b60a5adcb7ce3b2193d9ae1403
netrishelat <shelatnetri92@gmail.com>
2025-03-12 21:17:13 -0400

    Update base path in Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ProjectLLM-FrontEnd/', // Replace <REPO_NAME> with your repository name
+  base: '', // Replace <REPO_NAME> with your repository name
   build: {
     outDir: 'dist'
   },
